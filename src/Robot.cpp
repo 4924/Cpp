1e4bc8217d2fd6e0f6c3154e38edf65f90119e89
Elias Basler <e.e.basler@gmail.com>
2016-09-29 11:42:27 -0400

    base

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,41 +1,76 @@
 #include "WPILib.h"
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
-class Robot: public SampleRobot
-{
-	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
 
-public:
-	Robot() :
-			myRobot(0, 1),	// initialize the RobotDrive to use motor controllers on ports 0 and 1
-			stick(0)
+#include "WPILib.h"
+
+class Robot: public IterativeRobot
+{
+private:
+	LiveWindow *lw = LiveWindow::GetInstance();
+	SendableChooser *chooser;
+	const std::string autoNameDefault = "Default";
+	const std::string autoNameCustom = "My Auto";
+	std::string autoSelected;
+	RobotDrive robot = RobotDrive(0, 1);
+	Joystick stick = Joystick(0);
+
+	void RobotInit()
 	{
-		myRobot.SetExpiration(0.1);
+		chooser = new SendableChooser();
+		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
+		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
+		SmartDashboard::PutData("Auto Modes", chooser);
+		robot.SetExpiration(0.1);
 	}
 
+
 	/**
-	 * Runs the motors with arcade steering.
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
-	void OperatorControl()
+	void AutonomousInit()
 	{
-		while (IsOperatorControl() && IsEnabled())
-		{
-			myRobot.ArcadeDrive(stick.GetY(), -stick.GetX()); // drive with arcade style (use right stick)
-			Wait(0.005);				// wait for a motor update time
+		autoSelected = *((std::string*)chooser->GetSelected());
+		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
+		std::cout << "Auto selected: " << autoSelected << std::endl;
+
+		if(autoSelected == autoNameCustom){
+			//Custom Auto goes here
+		} else {
+			//Default Auto goes here
 		}
 	}
 
+	void AutonomousPeriodic()
+	{
+		if(autoSelected == autoNameCustom){
+			//Custom Auto goes here
+		} else {
+			//Default Auto goes here
+		}
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic()
+	{
+		robot.ArcadeDrive(stick.GetY(), -stick.GetX());
+	}
+
+	void TestPeriodic()
+	{
+		lw->Run();
+	}
 };
 
 START_ROBOT_CLASS(Robot)
+
 
